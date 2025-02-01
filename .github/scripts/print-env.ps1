540a1f717edc755d38ae1d6c73a0106a46c6af02
gcabrera-ddc <157718848+gcabrera-ddc@users.noreply.github.com>
2025-02-01 14:31:27 -0500

    Updated PowerShell script to print SAPI_KEY

--- .github/scripts/print-env.ps1
+++ .github/scripts/print-env.ps1
@@ -2,3 +2,10 @@
 
 Write-Output "Printing GitHub Environment Variables:"
 Get-ChildItem -Path Env: | Sort-Object Name | ForEach-Object { Write-Output "$($_.Name) = $($_.Value)" }
+
+# Print SAPI_KEY separately (if it exists)
+if ($env:SAPI_ACCOUNT_ID) {
+    Write-Output "SAPI_KEY = $env:SAPI_ACCOUNT_ID"
+} else {
+    Write-Output "SAPI_KEY is not set."
+}
