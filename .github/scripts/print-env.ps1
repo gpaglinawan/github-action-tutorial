e976267efc0b760107e67b1e1e64926de9e15b22
gcabrera-ddc <157718848+gcabrera-ddc@users.noreply.github.com>
2025-02-01 14:59:04 -0500

    Updated powershell

--- .github/scripts/print-env.ps1
+++ .github/scripts/print-env.ps1
@@ -3,9 +3,13 @@
 Write-Output "Printing GitHub Environment Variables:"
 Get-ChildItem -Path Env: | Sort-Object Name | ForEach-Object { Write-Output "$($_.Name) = $($_.Value)" }
 
+[System.Environment]::GetEnvironmentVariable("SAPI_KEY", "Process")
+[System.Environment]::GetEnvironmentVariable("SAPI_KEY", "User")
+[System.Environment]::GetEnvironmentVariable("SAPI_KEY", "Machine")
+
 # Print SAPI_KEY separately (if it exists)
 if ($env:SAPI_ACCOUNT_ID) {
-    Write-Output "SAPI_KEY = $env:SAPI_ACCOUNT_ID"
+    Write-Output "SAPI_KEY = $env:SAPI_ACCOUNT_IDgit add .github/scripts/print-env.ps1"
 } else {
     Write-Output "SAPI_KEY is not set."
 }
